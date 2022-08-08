4ba202bdc727d8efcab000262e7a2decc0d3bfb0
xlboy <xlboy88888@foxmail.com>
2022-08-08 21:00:34 +0800

    feat: 新增 `runtypes`，解决 `不同的入参类型匹配` 问题

---
+++ scripts/runtypes-schema-generator.ts
@@ -0,0 +1,17 @@
+import { Generator } from '@runtyping/runtypes';
+import { resolve } from 'node:path';
+
+const generator = new Generator({
+  targetFile: resolve(__dirname, '../src/constants/runtypes-schema.ts'),
+  runtypeFormat: 'RuntypesSchema{type}'
+});
+
+generator
+  .generate([
+    {
+      file: resolve(__dirname, '../src/types.ts'),
+      type: ['CommonKey', 'KeySequence'],
+      exportStaticType: false
+    }
+  ])
+  .then(file => file.save());
