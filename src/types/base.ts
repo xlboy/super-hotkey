7ea260a354b5f25e3b3230cebb64f4fadb9ff936
xlboy <xlboy88888@foxmail.com>
2022-08-16 00:53:35 +0800

    types: 新增 `ExtractFunctionFromPolymorphic`

--- src/types/base.ts
+++ src/types/base.ts
@@ -1,3 +1,9 @@
 export function defineVariables<T>() {
   return <C extends T>(value: C) => value;
 }
+
+export type ExtractFunctionFromPolymorphic</* Polymorphic-Type */ PT> = PT extends {
+  (...args: infer A): infer R;
+}
+  ? (...args: A) => R
+  : never;
